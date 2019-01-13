ef546b9bc376f73e8b5d7871ae35e06e17b5cea3
Mujadid Ali <mujadidali23@gmail.com>
2019-01-13 22:21:48 +0500

    Chapter3 First Heroku Commit

--- pagesproject/pages_project/settings.py
+++ pagesproject/pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's92ellcz3kstou($g#@!u-wzzpoh_a==h=cnzw!4f=4zo%ic!3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
